096bee364d8fee3f558419be87adcfdacc1b3336
sppantg <72460440+sppantg@users.noreply.github.com>
2023-06-15 18:36:14 +0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,16 @@
+$(document).ready(function () {
+  // Get the HTML form element
+  const formElement = $("#email-form-4");
 
+  // Create a new div element
+  const divElement = $("<div>");
+
+  // Set the class attribute of the div element
+  divElement.attr("class", formElement.attr("class"));
+
+  // Set the inner HTML of the div element to the HTML content of the form element
+  divElement.html(formElement.html());
+
+  // Replace the form element with the new div element
+  formElement.replaceWith(divElement);
+});
